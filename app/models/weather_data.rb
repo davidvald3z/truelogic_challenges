e465967918d5daa27557a8c3cfbc278d5c26e5d4
David Valdez <david_valdez@hotmail.com>
2024-09-03 17:16:08 -0600

    Intialization fixes

--- app/models/weather_data.rb
+++ app/models/weather_data.rb
@@ -3,8 +3,8 @@ module WeatherData
         file_path = "lib/data/w_data.dat"
 
         if File.exist?(file_path)
-            min_temp_spread = 999
-            min_temp_spread_day = 0
+            min_temp_spread = nil
+            min_temp_spread_day = nil
 
             File.foreach(file_path) do |line|
                 day = line.slice(2,4)
@@ -16,12 +16,12 @@ module WeatherData
 
                 temp_diff = get_clean_temp(max) - get_clean_temp(min)
                 
-                if temp_diff < min_temp_spread
+                if min_temp_spread_day.nil? || temp_diff < min_temp_spread
                     min_temp_spread_day = day
                     min_temp_spread = temp_diff
                 end
             end
-            if min_temp_spread_day == 0
+            if min_temp_spread_day.nil?
                 result = "Not valid records detected"
             else
                 result = "The day with the smallest temperature spread is: #{min_temp_spread_day}"
